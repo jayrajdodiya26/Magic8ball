4db0779cdd0ab42728cb86a9603e0e6dd925292c
joy <jayrajdodiya45@gmail.com>
2018-06-12 20:23:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/joy/magic8ball/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.joy.magic8ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView balldisplay=findViewById(R.id.image_eightball);
+       final int[] ballarray=new int[]
+                {
+                        R.drawable.ball1,
+                        R.drawable.ball2,
+                        R.drawable.ball3,
+                        R.drawable.ball4,
+                        R.drawable.ball5
+                };
+
+        Button mybutton=findViewById(R.id.askbutton);
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomnumber=new Random();
+                int number=randomnumber.nextInt(5);
+
+                balldisplay.setImageResource(ballarray[number]);
+            }
+        });
+
+    }
+}
